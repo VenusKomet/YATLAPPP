11a829bc4572068cd5b2440dc5d3aadd6b8f27ad
venuskomet <venuskomet@gmail.com>
2026-06-13 21:55:04 +0200

    feat(main): added initial API endpoints for the flask backend API and also defined some first helper functions

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,18 @@ from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
 
-# initial IDs
-todo_list_1_id = '1318d3d1-d979-47e1-a225-dab1751dbe75'
-todo_list_2_id = '3062dc25-6b80-4315-bb1d-a7c86b014c65'
-todo_list_3_id = '44b02e00-03bc-451d-8d01-0c67ea866fee'
+def generate_id():
+    return str(uuid.uuid4())
 
-todo_1_id = uuid.uuid4()
-todo_2_id = uuid.uuid4()
-todo_3_id = uuid.uuid4()
-todo_4_id = uuid.uuid4()
+# initial IDs
+todo_list_1_id = generate_id()
+todo_list_2_id = generate_id()
+todo_list_3_id = generate_id()
+
+todo_1_id = generate_id()
+todo_2_id = generate_id()
+todo_3_id = generate_id()
+todo_4_id = generate_id()
 
 # data
 todo_lists = [
@@ -19,129 +22,114 @@ todo_lists = [
     {'id': todo_list_2_id, 'name': 'Arbeit'},
     {'id': todo_list_3_id, 'name': 'Privat'},
 ]
+
 todos = [
     {'id': todo_1_id, 'name': 'Milch', 'description': '', 'list': todo_list_1_id},
     {'id': todo_2_id, 'name': 'Arbeitsblätter ausdrucken', 'description': '', 'list': todo_list_2_id},
     {'id': todo_3_id, 'name': 'Kinokarten kaufen', 'description': '', 'list': todo_list_3_id},
-    {'id': todo_3_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id},
+    {'id': todo_4_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id},
 ]
+
+# ------------------ helpers ------------------
+
+def find_list(list_id):
+    return next((l for l in todo_lists if l['id'] == list_id), None)
+
+def find_todo(entry_id):
+    return next((t for t in todos if t['id'] == entry_id), None)
 
 # ------------------ CORS ------------------
 
 @app.after_request
 def apply_cors_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE,PATCH'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
 
 # ------------------ routes ------------------
 
 @app.route('/todo-list', methods=['GET'])
-def getTodoList():
+def get_todo_lists():
     return jsonify(todo_lists), 200
 
+
 @app.route('/todo-list', methods=['POST'])
-def createTodoList():
+def create_todo_list():
     new_list = request.get_json(force=True)
-
-    print('Got new Task to add new List: {}'.format(new_list))
-
-    new_list['id'] = uuid.uuid4()
+    new_list['id'] = generate_id()
     todo_lists.append(new_list)
-
     return jsonify(new_list), 201
 
+
 @app.route('/todo-list/<list_id>', methods=['GET'])
-def getTodoListEntries(list_id):
-    list_item = None
-
-    for todolist in todo_lists:
-        if todolist['id'] == list_id:
-            list_item = todolist
-            break
-
-    if not list_item:
+def get_todo_list_entries(list_id):
+    if not find_list(list_id):
         abort(404)
 
-    print('returning todolist...')
-    return jsonify(item for item in todos if item['list'] == list_id), 200
+    result = [item for item in todos if item['list'] == list_id]
+    return jsonify(result), 200
+
 
 @app.route('/todo-list/<list_id>', methods=['DELETE'])
-def deleteTodoList(list_id):
-    list_item = None
-
-    for todolist in todo_lists:
-        if todolist['id'] == list_id:
-            list_item = todolist
-            break
+def delete_todo_list(list_id):
+    list_item = find_list(list_id)
 
     if not list_item:
         abort(404)
 
-    print('Deleting todo list...')
     todo_lists.remove(list_item)
+
+    # also remove associated todos
+    global todos
+    todos = [t for t in todos if t['list'] != list_id]
+
     return '', 204
 
+
 @app.route('/todo-list/<list_id>', methods=['POST'])
-def addTodoListEntry(list_id):
-    list_item = None
-
-    for todolist in todo_lists:
-        if todolist['id'] == list_id:
-            list_item = todolist
-            break
-
-    if not list_item:
+def add_todo_list_entry(list_id):
+    if not find_list(list_id):
         abort(404)
 
     new_todo = request.get_json(force=True)
-    print('Got new Task for adding a new Task to a List: {}'.format(new_todo))
-
-    new_todo['id'] = uuid.uuid4()
+    new_todo['id'] = generate_id()
     new_todo['list'] = list_id
 
     todos.append(new_todo)
     return jsonify(new_todo), 201
 
+
 @app.route('/todo-list/entry/<entry_id>', methods=['PATCH'])
-def modifyTodolistEntry(entry_id):
-    entry_item = None
+def modify_todo_list_entry(entry_id):
+    entry = find_todo(entry_id)
 
-    for entry in todos:
-        if entry['id'] == entry_id:
-            entry_item = entry
-            break
-
-    if not entry_item:
+    if not entry:
         abort(404)
 
-    modified_todo = request.get_json(force=True)
-    print('Got new Task to modify a Todo: {}'.format(modified_todo))
+    updates = request.get_json(force=True)
 
-    for entry in todos:
-        if entry['id'] == entry_id:
-            entry = modified_todo
-            break
+    # update only allowed fields
+    entry.update({
+        'name': updates.get('name', entry['name']),
+        'description': updates.get('description', entry['description'])
+    })
+
+    return jsonify(entry), 200
+
 
 @app.route('/todo-list/entry/<entry_id>', methods=['DELETE'])
-def deleteTodoListEntry(list_id):
-    list_item = None
+def delete_todo_list_entry(entry_id):
+    entry = find_todo(entry_id)
 
-    for todolist in todo_lists:
-        if todolist['id'] == list_id:
-            list_item = todolist
-            break
-
-    if not list_item:
+    if not entry:
         abort(404)
 
-    print('Deleting todo list...')
-    todo_lists.remove(list_item)
+    todos.remove(entry)
     return '', 204
+
 
 # ------------------ run ------------------
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host='0.0.0.0', port=5000)
